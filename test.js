3541590c526867215091c23a31e52f5c8e86cc15
x0r2 <x0r2>
2017-11-07 19:41:36 +0300

    Add tests

--- test.js
+++ test.js
@@ -2,24 +2,75 @@ import Etherscan from './index';
 
 const etherscan = new Etherscan();
 
-const accountMatch = expect.stringMatching(/^0x[0-9a-f]{40}$/);
-const balanceMatch = expect.stringMatching(/^[0-9]+$/);
+const address = '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae';
+const block = '4266461';
+
+const accountMatch = expect.stringMatching(/^0x[\da-f]{40}$/);
+const digitsMatch = expect.stringMatching(/^\d+$/);
 
 describe('account', () => {
     test('get ether balance', async () => {
         expect(await etherscan.getEtherBalance({
-            address: '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae'
-        })).toEqual(balanceMatch);
+            address
+        })).toEqual(digitsMatch);
     });
 
     test('get ether balance multi', async () => {
         const data = await etherscan.getEtherBalanceMulti({
-            address: ['0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae']
+            address
         });
 
         expect(data).toMatchObject([{
             account: accountMatch,
-            balance: balanceMatch,
+            balance: digitsMatch,
         }]);
+    });
+
+    describe('get transactions', async () => {
+        test('latest', async () => {
+            const data = await etherscan.getTxList({
+                address
+            });
+
+            expect(data).toContainEqual(
+                expect.objectContaining({
+                    blockNumber: digitsMatch
+                })
+            );
+        });
+
+        test('with start block', async () => {
+            const data = await etherscan.getTxList({
+                address,
+                startBlock: block
+            });
+
+            expect(data[0]).toMatchObject({
+                blockNumber: block
+            });
+        });
+
+        test('with end block', async () => {
+            const data = await etherscan.getTxList({
+                address,
+                endBlock: block
+            });
+
+            expect(data[data.length - 1]).toMatchObject({
+                blockNumber: block
+            });
+        });
+
+        test('with start and end blocks', async () => {
+            const data = await etherscan.getTxList({
+                address,
+                startBlock: block,
+                endBlock: block
+            });
+
+            expect(data).toMatchObject([{
+                blockNumber: block
+            }]);
+        });
     });
 });
