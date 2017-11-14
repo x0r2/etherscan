0381e991719e320d07f923a633e5ec2a691e80d5
x0r2 <x0r2>
2017-11-14 18:24:58 +0300

    Fix tests

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ import Etherscan from './index';
 const etherscan = new Etherscan();
 
 const address = '0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae';
-const block = '4266461';
+
+const startBlock = '4266461';
+const endBlock = '4275952';
 
 const accountMatch = expect.stringMatching(/^0x[\da-f]{40}$/);
 const digitsMatch = expect.stringMatching(/^\d+$/);
@@ -42,35 +44,58 @@ describe('account', () => {
         test('with start block', async () => {
             const data = await etherscan.getTxList({
                 address,
-                startBlock: block
+                startBlock
             });
 
             expect(data[0]).toMatchObject({
-                blockNumber: block
+                blockNumber: startBlock
             });
         });
 
         test('with end block', async () => {
             const data = await etherscan.getTxList({
                 address,
-                endBlock: block
+                endBlock: startBlock
             });
 
             expect(data[data.length - 1]).toMatchObject({
-                blockNumber: block
+                blockNumber: startBlock
             });
         });
 
         test('with start and end blocks', async () => {
             const data = await etherscan.getTxList({
                 address,
-                startBlock: block,
-                endBlock: block
+                startBlock,
+                endBlock: startBlock
             });
 
             expect(data).toMatchObject([{
-                blockNumber: block
+                blockNumber: startBlock
             }]);
+        });
+
+        test('sort', async () => {
+            const data = await etherscan.getTxList({
+                address,
+                startBlock,
+                endBlock
+            });
+
+            const dataDesc = await etherscan.getTxList({
+                address,
+                startBlock,
+                endBlock,
+                sort: 'desc'
+            });
+
+            expect(data[0]).toMatchObject({
+                blockNumber: startBlock
+            });
+
+            expect(dataDesc[0]).toMatchObject({
+                blockNumber: endBlock
+            });
         });
     });
 });
